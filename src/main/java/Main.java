cd4e4b28e1288d3ded9b315bf5b2872ff01b1217
izebr <i.zebrowski.ul@gmail.com>
2020-05-10 23:48:13 +0200

    added recursive counter method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,51 +3,81 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //program do obliczania ciągu Fibonacciego
+        Scanner scanner = new Scanner(System.in);
 
+        //iterative
         System.out.println("którą liczbę z ciągu Ci wyświetlić?");
 
-        Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
-        Long start = System.currentTimeMillis() / 1000;
-
-        System.out.println("Liczba wynosi " + fibonacci(x));
-
-
-        Long stop = System.currentTimeMillis() / 1000;
-        System.out.println(stop - start + " sec");
+        Long start0 = System.currentTimeMillis() / 1000;
+        System.out.println("Liczba wynosi " + iterativeFibonacci(x));
+        Long stop0 = System.currentTimeMillis() / 1000;
+        System.out.println(stop0 - start0 + " sec");
 
         System.out.println("które liczby ciągu chcesz do siebie dodać?");
-
-        int firstFib = scanner.nextInt();
+        int firstItFib = scanner.nextInt();
         System.out.println("oraz którą?");
-        int secondFib = scanner.nextInt();
-        if ((firstFib == 6 && secondFib == 9) || (firstFib == 9 && secondFib == 6)) {
+        int secondItFib = scanner.nextInt();
+        if ((firstItFib == 6 && secondItFib == 9) || (firstItFib == 9 && secondItFib == 6)) {
             System.out.println("jest to odpowiedź na \"Wielkie pytanie o życie, wszechświat i całą resztę\"");
         }
-        System.out.println("Suma wynosi : " + BigInteger.valueOf(Long.parseLong(fibonacci(firstFib)))
-                .add(BigInteger.valueOf(Long.parseLong(fibonacci(secondFib)))));
+        System.out.println("Suma wynosi : " + new BigInteger(iterativeFibonacci(firstItFib))
+                .add(new BigInteger(iterativeFibonacci(secondItFib))));
+
+
+        //recursive
+        System.out.println("którą liczbę z ciągu Ci wyświetlić?");
+
+        int y = scanner.nextInt();
+        Long start1 = System.currentTimeMillis() / 1000;
+        System.out.println("Liczba wynosi " + recursiveFibonacci(y));
+        Long stop1 = System.currentTimeMillis() / 1000;
+        System.out.println(stop1 - start1 + " sec");
+
+        System.out.println("które liczby ciągu chcesz do siebie dodać?");
+        int firstRecFib = scanner.nextInt();
+        System.out.println("oraz którą?");
+        int secondRecFib = scanner.nextInt();
+        if ((firstRecFib == 6 && secondRecFib == 9) || (firstRecFib == 9 && secondRecFib == 6)) {
+            System.out.println("jest to odpowiedź na \"Wielkie pytanie o życie, wszechświat i całą resztę\"");
+        }
+        System.out.println("Suma wynosi : " + BigInteger.valueOf(Long.parseLong(iterativeFibonacci(firstRecFib)))
+                .add(BigInteger.valueOf(Long.parseLong(iterativeFibonacci(secondRecFib)))));
+
+
     }
 
-    private static String fibonacci(int number) {
-        String[] fibonacciTab = new String[3];
-        if (number == 0) {
-            return "0";
-        }
-        if (number == 1 || number == 2) {
-            return "1";
+    private static String iterativeFibonacci(int number) {
+        String[] fibonacciTab = new String[number + 2];
+        if (number <= 1) {
+            return String.valueOf(number);
         } else {
             fibonacciTab[0] = "0";
             fibonacciTab[1] = "1";
             for (int i = 2; i <= number; i++) {
-                fibonacciTab[2] = String
-                        .valueOf(new BigInteger(fibonacciTab[0])
-                                .add(new BigInteger(fibonacciTab[1])));
-                fibonacciTab[0] = fibonacciTab[1];
-                fibonacciTab[1] = fibonacciTab[2];
+                fibonacciTab[i] = String
+                        .valueOf(new BigInteger(fibonacciTab[i - 1])
+                                .add(new BigInteger(fibonacciTab[i - 2])));
             }
 
-            return fibonacciTab[2];
+            return fibonacciTab[number];
         }
+
+    }
+
+    private static String recursiveFibonacci(int number) {
+        // bad idea for use above 43
+
+        if (number == 1 || number ==0) {
+            return String.valueOf(number);
+
+        }
+        else{
+        return String
+                .valueOf(new BigInteger(recursiveFibonacci(number - 1))
+                        .add(new BigInteger(recursiveFibonacci(number - 2))));
+
+    }
+
     }
 }
